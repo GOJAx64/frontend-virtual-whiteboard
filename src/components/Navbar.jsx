8960089dc654da01f255ad240cb6d07faf0f043b
GOJAx64 <josealbertogarcia453@gmail.com>
2023-01-13 14:38:18 -0600

    Responsive navbar in HomePage

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export const Navbar = () => {
+
+    const [toggleMenu, setToggleMenu] = useState(true);
+
+    const onClickButton = () => {
+        setToggleMenu(!toggleMenu);
+    }
+
+    return (
+        <nav className="flex items-center justify-between flex-wrap bg-veryDarkBlue mx-auto p-6 w-full">
+            
+            <div className="flex items-center flex-shrink-0 mr-6">
+                <p className="uppercase text-grayishBlue text-lg md:text-xl tracking-widest ">
+                    Pizarrones <span className='text-softRed'>Virtuales</span>
+                </p>
+            </div>
+
+            <div className="block lg:hidden">
+                <button onClick={ onClickButton } className="flex items-center px-3 py-2 border border-blue-500 rounded text-white hover:text-white hover:border-blue-500">
+                    <svg className="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
+                </button>
+            </div>
+
+            <div id='menuNavbar' className={ `w-full block flex-grow lg:flex lg:items-center lg:w-auto text-center lg:text-right uppercase text-grayishBlue ${ toggleMenu ? 'hidden' : '' }`}>
+                <div className="lg:flex-grow lg:space-x-7">
+                    <a onClick={ onClickButton } href="#features"     className="mt-4 lg:mt-0 tracking-widest hover:text-softRed block lg:inline-block">Características</a>
+                    <a onClick={ onClickButton } href="#technologies" className="mt-4 lg:mt-0 tracking-widest hover:text-softRed block lg:inline-block">Tecnologías</a>
+                    <a onClick={ onClickButton } href="#faq"          className="mt-4 lg:mt-0 tracking-widest hover:text-softRed block lg:inline-block">FAQ</a>
+                    <Link to="/login"    className="mt-4 lg:mt-0 px-8 py-2 text-white bg-softRed border-2 border-softRed rounded-lg shadow-md  hover:bg-red-400 block lg:inline-block">Iniciar Sesión</Link>
+                    <Link to="/register" className="mt-4 lg:mt-0 px-8 py-2 text-white bg-softRed border-2 border-softRed rounded-lg shadow-md  hover:bg-red-400 block lg:inline-block">Registrarse</Link>
+                </div>
+            </div>
+        </nav>
+    )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,53 +5,13 @@ import Feature3 from '../assets/illustration-features-tab-3.svg'
 import ReactIcon from '../assets/react.svg';
 import NodeIcon from '../assets/node.svg';
 import MysqlIcon from '../assets/mysql.svg';
-import Logo from '../assets/logo.svg';
+import { Navbar } from '../components/Navbar';
 
 export const Home = () => {
   return (
     <div className='overflow-x-hidden bg-slate-100'>
-      <nav className="realtive bg-veryDarkBlue mx-auto py-2 px-6">
-      
-        <div className="flex items-center justify-between space-x-20 my-6">
-        
-          <div className="z-30">
-             {/* <img src={ Logo } alt="logo" id="logo" /> */}
-            <p className="uppercase text-grayishBlue text-xl tracking-widest ">
-              Pizarrones <span className='uppercase text-softRed text-xl tracking-widest'>Virtuales</span>
-            </p>
-          </div>
-
-          <div className="hidden items-center space-x-10 uppercase text-grayishBlue md:flex">
-            <a href="#features" className="tracking-widest hover:text-softRed">Características</a>
-            <a href="#technologies" className="tracking-widest hover:text-softRed">Tecnologías</a>
-            <a href="#faq" className="tracking-widest hover:text-softRed">FAQ</a>
-            <Link to="/login" className="px-8 py-2 text-white bg-softRed border-2 border-softRed rounded-lg shadow-md  hover:bg-red-400">Iniciar Sesión</Link>
-            <Link to="/register" className="px-8 py-2 text-white bg-softRed border-2 border-softRed rounded-lg shadow-md  hover:bg-red-400">Registrarse</Link>
-          </div>
-        
-          <button id="menu-btn" className="z-30 block md:hidden focus:outline-none hamburger">
-            <span className="hamburger-top"></span>
-            <span className="hamburger-middle"></span>
-            <span className="hamburger-bottom"></span>
-          </button>
-        </div>
-      
-        <div id="menu" className="fixed inset-0 z-20 hidden flex-col items-center self-end w-full h-full m-h-screen px-6 py-1 pt-24 pb-4 tracking-widest text-white uppercase divide-y divide-gray-500 opacity-90 bg-veryDarkBlue">
-          <div className="w-full py-3 text-center">
-            <a href="#features" className="block hover:text-softRed">Características</a>
-          </div>
-          <div className="w-full py-3 text-center">
-            <a href="#download" className="block hover:text-softRed">Tecnologías</a>
-          </div>
-          <div className="w-full py-3 text-center">
-            <a href="#faq" className="block hover:text-softRed">FAQ</a>
-          </div>
-          <div className="w-full py-3 text-center">
-            <a href="#" className="block hover:text-softRed">Login</a>
-          </div>
-        </div>
-      </nav>
-
+      <Navbar/>
+    
       <section id="hero">
         <div className="container flex flex-col-reverse mx-auto p-6 lg:flex-row lg:mb-0">
           <div className="flex flex-col space-y-10 lg:mt-16 lg:w-1/2">
@@ -175,10 +135,10 @@ export const Home = () => {
         </div>
       </section>
 
-      <section id="download">
+      <section id="technologies">
         <div className="container mx-auto px-6">
           <h2 className="mb-6 text-3xl font-semibold text-center md:text-4xl">
-            Con esto se construyo Pizarrones Virtuales
+            Con esto se construyó Pizarrones Virtuales
           </h2>
           <p className="max-w-lg mx-auto text-center text-grayishBlue">
             La aplicación consta de tres pilares. Una interfaz de usuario. Un Websocket 
@@ -188,51 +148,45 @@ export const Home = () => {
         </div>
       </section>
 
-      <section id="download-boxes" className="py-12">
+      <section id="technologies-boxes" className="py-12">
         <div className="relative flex flex-col items-center max-w-5xl mx-auto space-y-10 px-10 md:px-6 md:space-y-0 md:space-x-7 md:flex-row">
 
-          <div className="flex flex-col w-full py-6 space-y-4 text-center rounded-lg shadow-lg md:w-1/3 border border-slate-200">
+          <div className="w-full md:w-1/3 border bg-slate-100 border-slate-300 rounded-lg shadow-lg flex flex-col py-6 space-y-4 text-center">
             <div className="flex justify-center">
               <img src={ ReactIcon } alt="React JS" className='h-24'/>
             </div>
     
             <h5 className="pt-6 text-xl font-bold">React JS</h5>
-            <p className="text-gray-400">Para la interfaz de la aplicación, el usuario interactura directamente</p>
+            <p className="text-gray-400 mx-2">Para la interfaz de la aplicación, el usuario interactura directamente</p>
 
-            <div className="bg-dots bg-repeat-x px-6 pt-6 capitalize">
-              <a href="#" className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-softBlue hover:text-softBlue hover:bg-white border-softBlue">Add & Install Extension</a>
+            <div className="bg-repeat-x px-6 pt-6 capitalize">
+              <a href="#" className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-softBlue hover:text-softBlue hover:bg-white border-softBlue">mas información</a>
             </div>
           </div>
 
-          <div className="w-full md:w-1/3 border border-slate-200 rounded-lg">
-            <div className="flex flex-col w-full py-6 space-y-4 text-center rounded-lg shadow-lg ">
-      
-              <div className="flex justify-center">
-                <img src={ NodeIcon } alt="Node JS" className='h-32'/>
-              </div>
-      
-              <h5 className="pt-6 text-xl font-bold">Node JS</h5>
-              <p className="text-gray-400">WebSocket Rest que expone los recursos para comunicarse con el frontend</p>
+          <div className="w-full md:w-1/3 border bg-slate-100 border-slate-300 rounded-lg shadow-lg flex flex-col py-6 space-y-4 text-center">
+            <div className="flex justify-center">
+              <img src={ NodeIcon } alt="Node JS" className='h-24'/>
+            </div>
+    
+            <h5 className="pt-6 text-xl font-bold">Node JS</h5>
+            <p className="text-gray-400 mx-2">WebSocket Rest que expone los recursos al frontend</p>
 
-              <div className="bg-dots bg-repeat-x px-6 pt-6 capitalize">
-                <a href="#" className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-softBlue hover:text-softBlue hover:bg-white border-softBlue">Add & Install Extension</a>
-              </div>
+            <div className="bg-repeat-x px-6 pt-6 capitalize">
+              <a href="#" className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-softBlue hover:text-softBlue hover:bg-white border-softBlue">mas información</a>
             </div>
           </div>
 
-          <div className="w-full md:w-1/3 border border-slate-200 rounded-lg">
-            <div className="flex flex-col w-full py-6 space-y-4 text-center rounded-lg shadow-lg">
-              
-              <div className="flex justify-center">
-                <img src={ MysqlIcon } alt="Tensorflow" className='h-24'/>
-              </div>
+          <div className="w-full md:w-1/3 border bg-slate-100 border-slate-300 rounded-lg shadow-lg flex flex-col py-6 space-y-4 text-center">
+            <div className="flex justify-center">
+              <img src={ MysqlIcon } alt="Tensorflow" className='h-24'/>
+            </div>
+    
+            <h5 className="pt-6 text-xl font-bold">Tensorflow</h5>
+            <p className="text-gray-400 mx-2">Para la detección de gestos en tiempo real</p>
 
-              <h5 className="pt-6 text-xl font-bold">Tensorflow</h5>
-              <p className="text-gray-400">Para la detección de gestos en tiempo real</p>
-
-              <div className="bg-dots bg-repeat-x px-6 pt-6 capitalize">
-                <a href="#" className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-softBlue hover:text-softBlue hover:bg-white border-softBlue">Add & Install Extension</a>
-              </div>
+            <div className="bg-repeat-x px-6 pt-6 capitalize">
+              <a href="#" className="block w-full py-3 text-white duration-200 border-2 rounded-lg bg-softBlue hover:text-softBlue hover:bg-white border-softBlue">mas información</a>
             </div>
           </div>
 
