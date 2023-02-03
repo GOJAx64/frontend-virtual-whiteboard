76a3205de7ebca0773f203884ca7b03fe009a91e
GOJAx64 <josealbertogarcia453@gmail.com>
2023-02-02 23:44:51 -0600

    Styling classroom page

--- src/components/OptionsBar.jsx
+++ src/components/OptionsBar.jsx
@@ -2,7 +2,7 @@ import Gesture from '../assets/gesture.svg';
 import Symbols from '../assets/symbol.svg';
 import Profile from '../assets/profile.svg';
 import Add from '../assets/add2.svg';
-import Dashboard from '../assets/d1.svg';
+import Dashboard  from '../assets/home.svg';
 import { Link } from 'react-router-dom';
 
 export const OptionsBar = () => {
@@ -10,7 +10,7 @@ export const OptionsBar = () => {
     <aside className=' h-screen bg-slate-200 border-l border-t border-slate-300 md:w-12 py-5 shadow-lg' >
         {/* dark:bg-slate-800 dark:border-slate-800 */}
         <Link to="/dashboard">
-          <img src={ Dashboard } alt='Dashboard' className='mx-auto mb-6 h-4'/>
+          <img src={ Dashboard } alt='Dashboard' className='mx-auto mb-6 h-6'/>
         </Link>
         <Link to="gestures">
           <img src={ Gesture } alt='Gestures'    className='mx-auto my-6 h-8'/>
--- src/pages/app/Classroom.jsx
+++ src/pages/app/Classroom.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { useClassrooms } from "../../hooks"
 
 export const Classroom = () => {
@@ -12,9 +12,18 @@ export const Classroom = () => {
     
     const { id, name, description} = classroom;
     return (
-        <div>
-            <h1 className="text-xl text-slate-600">{ name }</h1>
-            <h1 className="text-md text-slate-500">{ description }</h1>
+        <div className="p-6">
+            <h1 className="inline text-xl text-softBlue uppercase">{ name }</h1>
+            <h3 className="text-md text-slate-400">{ description }</h3>
+            {/* <hr className='border border-slate-300 my-4'/> */}
+
+            <nav className="flex items-center justify-between flex-wrap mt-3 mx-auto px-6 w-full">
+                <Link to="summary"            className="mt-4 text-sm text-slate-400 tracking-widest hover:text-softRed block lg:inline-block uppercase">resumen</Link>
+                <Link to="whiteboards"        className="mt-4 text-sm text-slate-400 tracking-widest hover:text-softRed block lg:inline-block uppercase">pizarrones</Link>
+                <Link to="chat"               className="mt-4 text-sm text-slate-400 tracking-widest hover:text-softRed block lg:inline-block uppercase">chat</Link>
+                <Link to="classroom_settings" className="mt-4 text-sm text-slate-400 tracking-widest hover:text-softRed block lg:inline-block uppercase">configuración</Link>
+            </nav>
+            <hr className='border border-slate-300 mt-1'/>
         </div>
     )
 }
