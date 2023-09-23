3871b3b68fa383419bb50c2346180a374a4b6259
Alberto Garcia <josealbertogarcia453@gmail.com>
2023-09-22 23:11:47 -0600

    Creting button for delete, update and sync text in an image

--- src/components/ImageInfo.jsx
+++ src/components/ImageInfo.jsx
@@ -20,10 +20,35 @@ export const ImageInfo = () => {
     const handleClick = () => {
         setShowModalImage(true);
     }
+
+    const handleDelete = () => {
+        console.log('delete');
+    }
+
+    const handleSubmit = () => {
+        console.log('save')
+    }
     
+    const handleSyncChars = () => {
+        console.log('sync chars')
+    }
+
     return (
         <div>
-            <img src={image.url} onClick={ handleClick } alt={'Capture'} className='border mx-auto w-8/12 my-2 rounded-sm'/>
+            <div className='flex'>
+                <div className='flex flex-col'>
+                    <button onClick={ handleDelete } className="border border-slate-200 p-2 my-1 mx-2 text-softRed pointer text-sm font-semibold rounded hover:cursor-pointer hover:border-softRed"> 
+                        Eliminar 
+                    </button>
+                    <button onClick={ handleSubmit } className="bg-softBlue p-2 my-1 mx-2 text-white pointer text-sm  font-semibold rounded hover:cursor-pointer hover:bg-blue-800 transition-colors">
+                        Guardar
+                    </button>
+                    <button onClick={ handleSyncChars } className="bg-softRed p-2 my-1 mx-2 text-slate-50 pointer text-sm  font-semibold rounded hover:cursor-pointer hover:bg-red-500"> 
+                        Sincronizar 
+                    </button>  
+                </div>
+                <img src={image.url} onClick={ handleClick } alt={'Capture'} className='border mx-auto w-8/12 my-2 rounded-sm'/>
+            </div>
             <div className="mt-5 mx-3 h-">
                 <div className='overflow-y-auto scrollbar-hide h-52'>
                     <ReactQuill theme="snow" value={text} onChange={handleText}/>
