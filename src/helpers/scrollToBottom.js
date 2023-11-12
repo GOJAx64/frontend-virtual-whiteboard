cf3ea2265dfcde380837ea83cfb714efd04f4feb
Alberto Garcia <josealbertogarcia453@gmail.com>
2023-11-12 13:36:34 -0600

    Eliminating clg in scrollToBottom

--- src/helpers/scrollToBottom.js
+++ src/helpers/scrollToBottom.js
@@ -1,15 +1,13 @@
 import { animateScroll } from 'react-scroll';
 
 export const scrollToBottom = (id) => {
-  console.log('scroll botton ', id)
   animateScroll.scrollToBottom({
     containerId: id,
     duration: 0
   })
 };
 
-export const scrollToBottomAnimated = (id) => {
-  console.log('scroll botton animated')  
+export const scrollToBottomAnimated = (id) => {  
   animateScroll.scrollToBottom({
       containerId: id,
       duration: 250
